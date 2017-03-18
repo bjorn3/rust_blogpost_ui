c1fb94ab480124c52d8da3226437260e051e3408
bjorn3 <bjorn3@users.noreply.github.com>
2017-03-18 14:26:56 +0100

    Fix build

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate cursive;
+#[macro_use]
+extern crate lazy_static;
+
 extern crate reqwest;
 extern crate scraper;
 
@@ -9,25 +12,32 @@ extern crate serde_derive;
 pub mod post;
 pub mod blogs;
 
+use std::sync::Mutex;
+
 use cursive::Cursive;
 use cursive::views::{TextView, ListView, SelectView};
 
+use post::Post;
+
+lazy_static! {
+    static ref POSTS: Mutex<Vec<Post>> = Mutex::new(Vec::new());
+}
+
 fn main() {
-    let mut posts = Vec::new();
-    posts.extend(blogs::manishearth::extract());
+    POSTS.lock().unwrap().extend(blogs::manishearth::extract());
     //posts.extend(blogs::steveklabnik::extract());
 
-    println!("{:#?}", posts);
+    println!("{:#?}", *POSTS.lock().unwrap());
 
     let mut siv = Cursive::new();
 
     let mut posts_view = SelectView::new()
-        .on_submit(|s, post: &post::Post| {
+        .on_submit(|s, &i| {
             let screen = s.add_active_screen();
-            s.add_fullscreen_layer(TextView::new(post.content.clone()));
+            s.add_fullscreen_layer(TextView::new(POSTS.lock().unwrap()[i as usize].content.clone()));
         });
-    for (i, post) in posts.iter().enumerate() {
-        posts_view.add_item(post.title.clone(), post.clone());
+    for (i, post) in POSTS.lock().unwrap().iter().enumerate() {
+        posts_view.add_item(post.title.clone(), i);
     }
 
     siv.add_fullscreen_layer(posts_view);
